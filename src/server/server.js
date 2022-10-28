83f4406694d154dca463a660380fa0ec4066da8e
Nicolas <79662506+Klipfel-Nicolas@users.noreply.github.com>
2022-10-28 13:25:03 +0200

    config template engine handelbars

--- src/server/server.js
+++ src/server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const fs = require('fs')
 const bodyParser = require('body-parser');
-var hbs = require('hbs');
+var hbs = require('express-handlebars');
 
 const app = express()
 
@@ -11,27 +11,45 @@ app.use(express.static(path.resolve(__dirname, '../../dist')))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.set('view engine', 'hbs');
+// Handelbars Config
 app.set('views', path.join(__dirname, '../views'));
+app.engine( 'hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, '../views'),
+    partialsDir: path.join(__dirname, '../views/partials/'),
+}))
+app.set('view engine', 'hbs');
 
-
-
+/**
+ * Home Page
+ */
 app.get('/', (req, res) => {
 
-    /* const pathToHtmlFile = path.resolve(__dirname, '../../dist/home.html')
-    const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8')
-
-    res.sendFile(contentFromHtmlFile); */
-
-    res.render('index.hbs')
+    res.render('main.hbs', {
+        title: 'Space Discovery',
+        template: 'home',
+        // Dynamic template handelbars
+        whichPartial: function() {
+            return "pages/home";
+        }
+    })
 })
 
+/**
+ * Perseverence Page
+ */
 app.get('/perseverence/', (req, res) => {
-    const pathToHtmlFile = path.resolve(__dirname, '../../dist/perseverence.html')
-    const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8')
 
-    res.send(contentFromHtmlFile)
+    res.render('main.hbs', {
+        title: 'Perseverence',
+        template: 'perseverence',
+        whichPartial: function() {
+            return "pages/perseverence";
+        }
+    })
 })
+
 
 app.listen(3000, () => {
     console.log('Application is running on http://localhost:3000/')
